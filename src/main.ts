a52f9c181cf70071b0d1fcc5dd7fe3f90bc201e4
Nazar Hlukhaniuk <gluhanuk98@gmail.com>
2024-12-29 20:28:06 +0100

    Setting main.ts(redis, cookie parser) and added 2 utils functions for time and parse to boolean

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,55 @@
-import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { RedisStore } from 'connect-redis';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
-import { AppModule } from './core/app.module'
+import { RedisService } from '@/src/core/redis/redis.service';
+import { ms, type StringValue } from '@/src/shared/utils/ms.util';
+import { parseBoolean } from '@/src/shared/utils/parse-boolean.util';
+
+import { AppModule } from './core/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.create(AppModule);
+
+	const config = app.get(ConfigService);
+	const redis = app.get(RedisService);
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposedHeaders: ['set-cookie'],
+	});
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(
+					config.getOrThrow<StringValue>('SESSION_EXPIRATION'),
+				),
+				httpOnly: parseBoolean(
+					config.getOrThrow<string>('SESSION_HTTP_ONLY'),
+				),
+				secure: parseBoolean(
+					config.getOrThrow<string>('SESSION_SECURE'),
+				),
+				sameSite: 'lax',
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+			}),
+		}),
+	);
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
-bootstrap()
+bootstrap();
